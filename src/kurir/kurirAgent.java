259005e7f89226445fd2cf6311ae50f32435b09d
unknown <daviddimas80@gmail.com>
2022-06-24 00:35:11 +0700

    add agent kurir

---
+++ src/kurir/kurirAgent.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package buyer;
+
+import jade.core.*;
+import jade.core.behaviours.*;
+import jade.lang.acl.*;
+
+import jade.domain.*;
+import jade.domain.FIPAAgentManagement.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import java.util.Vector;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.Scanner;
+import seller.BookSellerAgent;
+
+public class BookBuyerAgent extends Agent {
+    // The list of known seller agents   
+
+    private Vector sellerAgents = new Vector();
+
+    // The GUI to interact with the user   
+    private BookBuyerGui myGui;
+    public LinkedList title = new LinkedList();
+    public LinkedList category = new LinkedList();
+    public LinkedList qty = new LinkedList();
+
+    /**
+     * Agent initializations
+     *
+     */
+    protected void setup() {
+        // Printout a welcome message   
+        System.out.println("Buyer-agent " + getAID().getLocalName() + " is ready.");
+
+        // Get names of seller agents as arguments   
+        ReadDb();
+        System.out.println(title.size());
+
+        // Show the GUI to interact with the user   
+        myGui = new BookBuyerGuiImpl(title, category, qty);
+        myGui.setAgent(this);
+        myGui.show();
+        addBehaviour(new waitingBehaviour(this));
+    }
+
+    /**
+     * Agent clean-up
+     *
+     */
+    protected void takeDown() {
+        // Dispose the GUI if it is there   
+        if (myGui != null) {
+            myGui.dispose();
+        }
+
+        // Printout a dismissal message   
+        System.out.println("Buyer-agent " + getAID().getName() + "terminated.");
+    }
+
+    class waitingBehaviour extends CyclicBehaviour {
+
+        ACLMessage msg = receive();
+
+        public waitingBehaviour(Agent a) {
+            super(a);
+        }
+
+        public void action() {
+            msg = receive();
+            if (msg != null) {
+
+                System.out.println("new msg: " + msg.getContent());
+                String lastMsg = msg.getContent();
+                String[] tokens = lastMsg.split(" ");
+                title.add(tokens[0]);
+                category.add(tokens[1]);
+                qty.add(tokens[2]);
+
+            }
+        }
+    }
+
+    public void ReadDb() {
+        try {
+            File myObj = new File("bookDB.txt");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                this.title.add(myReader.nextLine());
+                this.category.add(myReader.nextLine());
+                this.qty.add(myReader.nextLine());
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/kurir/kurirGuiImpl.java
@@ -0,0 +1,282 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package buyer;
+
+/**
+ *
+ * @author USER
+ */
+import jade.gui.TimeChooser;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.border.*;
+
+import java.util.Date;
+import java.util.LinkedList;
+
+/**
+ * J2SE (Swing-based) implementation of the GUI of the agent that tries to buy
+ * books on behalf of its user
+ */
+public class BookBuyerGuiImpl extends JFrame implements BookBuyerGui {
+
+    private BookBuyerAgent myAgent;
+
+    private JTextField titleTF, desiredCostTF, maxCostTF, deadlineTF, address;
+    private JButton setDeadlineB;
+    private JButton setCCB, buyB, resetB, exitB;
+    private JButton pinjam;
+    private JTextArea logTA;
+    public JPanel k;
+     public int addy=1;
+
+    private Date deadline;
+
+    public BookBuyerGuiImpl(LinkedList title, LinkedList category, LinkedList qty) {
+        super();
+
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                myAgent.doDelete();
+            }
+        });
+
+        JPanel rootPanel = new JPanel();
+        rootPanel.setLayout(new GridBagLayout());
+        rootPanel.setMinimumSize(new Dimension(330, 125));
+        rootPanel.setPreferredSize(new Dimension(330, 125));
+
+        ///////////   
+        // Line 0   
+        ///////////   
+        JLabel l = new JLabel("Find Book:");
+        l.setHorizontalAlignment(SwingConstants.LEFT);
+        GridBagConstraints gridBagConstraints = new GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
+        gridBagConstraints.insets = new java.awt.Insets(5, 3, 0, 3);
+        rootPanel.add(l, gridBagConstraints);
+
+        titleTF = new JTextField(64);
+        titleTF.setMinimumSize(new Dimension(222, 20));
+        titleTF.setPreferredSize(new Dimension(222, 20));
+        gridBagConstraints = new GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.gridwidth = 3;
+        gridBagConstraints.anchor = GridBagConstraints.NORTHWEST;
+        gridBagConstraints.insets = new Insets(5, 3, 0, 3);
+        rootPanel.add(titleTF, gridBagConstraints);
+
+        ///////////   
+        // Line 2   
+        ///////////   
+        l = new JLabel("Category:");
+        l.setHorizontalAlignment(SwingConstants.LEFT);
+        gridBagConstraints = new GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 2;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
+        gridBagConstraints.insets = new java.awt.Insets(5, 3, 0, 3);
+        rootPanel.add(l, gridBagConstraints);
+
+        deadlineTF = new JTextField(64);
+        deadlineTF.setMinimumSize(new Dimension(146, 20));
+        deadlineTF.setPreferredSize(new Dimension(146, 20));
+
+        gridBagConstraints = new GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 2;
+        gridBagConstraints.gridwidth = 2;
+        gridBagConstraints.anchor = GridBagConstraints.NORTHWEST;
+        gridBagConstraints.insets = new Insets(5, 3, 0, 3);
+        rootPanel.add(deadlineTF, gridBagConstraints);
+
+        l = new JLabel("Name:");
+        l.setHorizontalAlignment(SwingConstants.LEFT);
+        gridBagConstraints = new GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 3;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
+        gridBagConstraints.insets = new java.awt.Insets(5, 3, 0, 3);
+        rootPanel.add(l, gridBagConstraints);
+
+        address = new JTextField(64);
+        address.setMinimumSize(new Dimension(146, 20));
+        address.setPreferredSize(new Dimension(146, 20));
+
+        gridBagConstraints = new GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 3;
+        gridBagConstraints.gridwidth = 2;
+        gridBagConstraints.anchor = GridBagConstraints.NORTHWEST;
+        gridBagConstraints.insets = new Insets(5, 3, 0, 3);
+        rootPanel.add(address, gridBagConstraints);
+        rootPanel.setBorder(new BevelBorder(BevelBorder.LOWERED));
+        getContentPane().add(rootPanel, BorderLayout.NORTH);
+
+        l = new JLabel("Address:");
+        l.setHorizontalAlignment(SwingConstants.LEFT);
+        gridBagConstraints = new GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 4;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
+        gridBagConstraints.insets = new java.awt.Insets(5, 3, 0, 3);
+        rootPanel.add(l, gridBagConstraints);
+
+        address = new JTextField(64);
+        address.setMinimumSize(new Dimension(146, 20));
+        address.setPreferredSize(new Dimension(146, 20));
+
+        gridBagConstraints = new GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 4;
+        gridBagConstraints.gridwidth = 2;
+        gridBagConstraints.anchor = GridBagConstraints.NORTHWEST;
+        gridBagConstraints.insets = new Insets(5, 3, 0, 3);
+        rootPanel.add(address, gridBagConstraints);
+        rootPanel.setBorder(new BevelBorder(BevelBorder.LOWERED));
+        getContentPane().add(rootPanel, BorderLayout.NORTH);
+
+        /////////////////////////PANEL 1/////////////////////////
+        /////////////////////////PANEL 2/////////////////////////
+        k = new JPanel();
+        k.setLayout(new GridBagLayout());
+        //k.setMinimumSize(new Dimension(330, 125));
+        //k.setPreferredSize(new Dimension(330, 125));
+
+        l = new JLabel("Title");
+        l.setHorizontalAlignment(SwingConstants.LEFT);
+        gridBagConstraints = new GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 1;
+        gridBagConstraints.ipadx = 6;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
+        //gridBagConstraints.insets = new java.awt.Insets(5, 3, 0, 3);  
+        k.add(l, gridBagConstraints);
+
+        l = new JLabel("Category");
+        l.setHorizontalAlignment(SwingConstants.LEFT);
+        gridBagConstraints = new GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 1;
+        gridBagConstraints.ipadx = 6;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
+        //gridBagConstraints.insets = new java.awt.Insets(5, 3, 0, 3);  
+        k.add(l, gridBagConstraints);
+
+        l = new JLabel("Quantity");
+        l.setHorizontalAlignment(SwingConstants.LEFT);
+        gridBagConstraints = new GridBagConstraints();
+        gridBagConstraints.gridx = 3;
+        gridBagConstraints.gridy = 1;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
+        //gridBagConstraints.insets = new java.awt.Insets(5, 3, 0, 3);  
+        k.add(l, gridBagConstraints);
+
+        l = new JLabel("Status");
+        l.setHorizontalAlignment(SwingConstants.LEFT);
+        gridBagConstraints = new GridBagConstraints();
+        gridBagConstraints.gridx = 5;
+        gridBagConstraints.gridy = 1;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
+        //gridBagConstraints.insets = new java.awt.Insets(5, 3, 0, 3);  
+        k.add(l, gridBagConstraints);
+
+        for (int i = 3; i < title.size() + 3; i++) {
+            l = new JLabel(title.get(i - 3).toString());
+            l.setHorizontalAlignment(SwingConstants.LEFT);
+            gridBagConstraints = new GridBagConstraints();
+            gridBagConstraints.gridx = 0;
+            gridBagConstraints.gridy = i;
+            gridBagConstraints.ipadx = 6;
+            gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
+            //gridBagConstraints.insets = new java.awt.Insets(5, 3, 0, 3);  
+            k.add(l, gridBagConstraints);
+
+            l = new JLabel(category.get(i - 3).toString());
+            l.setHorizontalAlignment(SwingConstants.LEFT);
+            gridBagConstraints = new GridBagConstraints();
+            gridBagConstraints.gridx = 1;
+            gridBagConstraints.gridy = i;
+            gridBagConstraints.ipadx = 6;
+            gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
+            //gridBagConstraints.insets = new java.awt.Insets(5, 3, 0, 3);  
+            k.add(l, gridBagConstraints);
+
+            l = new JLabel(qty.get(i - 3).toString());
+            l.setHorizontalAlignment(SwingConstants.LEFT);
+            gridBagConstraints = new GridBagConstraints();
+            gridBagConstraints.gridx = 3;
+            gridBagConstraints.gridy = i;
+            gridBagConstraints.ipadx = 6;
+            gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
+            //gridBagConstraints.insets = new java.awt.Insets(5, 3, 0, 3);  
+            k.add(l, gridBagConstraints);
+
+            JButton buyB = new JButton("Pinjam");
+            buyB.addActionListener(new ActionListener() {
+                int getIndex = addy - 3;
+
+                public void actionPerformed(ActionEvent e) {
+
+                }
+            });
+            gridBagConstraints = new GridBagConstraints();
+            gridBagConstraints.gridx = 5;
+            gridBagConstraints.gridy = i;
+            // gridBagConstraints.insets = new java.awt.Insets(5, 3, 0, 3);  
+            k.add(buyB, gridBagConstraints);
+            addy++;
+            getContentPane().add(k, BorderLayout.WEST);
+        }
+/////////////////////////PANEL 2/////////////////////////
+
+/////////////////////////PANEL 3/////////////////////////
+        JPanel p = new JPanel();
+
+        resetB = new JButton("Reset");
+        resetB.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                titleTF.setText("");
+                desiredCostTF.setText("");
+                maxCostTF.setText("");
+                deadlineTF.setText("");
+                deadline = null;
+            }
+        });
+        exitB = new JButton("Exit");
+        exitB.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                myAgent.doDelete();
+            }
+        });
+
+        exitB.setPreferredSize(resetB.getPreferredSize());
+
+        p.add(resetB);
+        p.add(exitB);
+
+        p.setBorder(new BevelBorder(BevelBorder.LOWERED));
+        getContentPane().add(p, BorderLayout.SOUTH);
+
+        pack();
+
+    }
+/////////////////////////PANEL 3/////////////////////////
+
+    public void setAgent(BookBuyerAgent a) {
+        myAgent = a;
+        setTitle(myAgent.getName());
+    }
+
+    public void notifyUser(String message) {
+        logTA.append(message + "\n");
+    }
+}
